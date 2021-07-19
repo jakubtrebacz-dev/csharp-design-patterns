537bf9576c908b36493b3f8650322c346e86a766
Jakub Trebacz <kubatrebacz@gmail.com>
2021-07-19 17:26:10 +0100

    Create a simple problem to demonstrate violation of Single Responsibility Principle

--- Code/SOLID Design Principles/Single Responsibility Principle/Program.cs
+++ Code/SOLID Design Principles/Single Responsibility Principle/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace Single_Responsibility_Principle
+namespace SOLID.SingleResponsibilityPrinciple
 {
+    public class Journal
+    {
+        private readonly List<string> entires = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entires.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index) 
+        {
+            entires.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entires);
+        }
+
+        // This violates single resposibility principle
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Journal journal = new Journal();
+
+            journal.AddEntry("Today I'm documenting my knowledge");
+            journal.AddEntry("It's going great");
+
+            Console.WriteLine(journal); // This works because we have override ToString
         }
     }
 }
