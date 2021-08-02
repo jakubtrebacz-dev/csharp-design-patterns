18d0c519aa92b3c27a9e0adea94228561b820cf7
Jakub Trebacz <kubatrebacz@gmail.com>
2021-08-02 13:26:46 +0100

    Example

---
+++ Code/SOLID Design Principles/SOLID.OpenClosePrinciple/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SOLID.OpenClosePrinciple
+{
+    public enum Colour 
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Colour Colour;
+        public Size Size;
+
+        public Product(string name, Colour colour, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+
+            Name = name;
+            Colour = colour;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product apple = new Product("Apple", Colour.Green, Size.Small);
+            Product tree = new Product("Tree", Colour.Green, Size.Medium);
+            Product house = new Product("House", Colour.Red, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            ProductFilter productFilter = new ProductFilter();
+            Console.WriteLine("Green products (old): "); 
+        }
+    }
+}
